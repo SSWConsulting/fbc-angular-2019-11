92921db056cdba2c150a15fdfd810dfc6bfe367c
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-19 13:36:34 +1100

    added CompanyService

---
+++ src/app/company/company.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { Company } from './company';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CompanyService {
+
+  constructor() { }
+
+  getCompaines(): Company[] {
+    return [
+      {name: 'Company A', email: 'companyA@ssw.com.au', phone: 1234},
+      {name: 'Company B', email: 'companyB@ssw.com.au', phone: 5678},
+      {name: 'Company C', email: 'companyC@ssw.com.au', phone: 9133},
+    ];
+  }
+
+}
