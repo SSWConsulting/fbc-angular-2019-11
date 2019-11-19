6517c8b86de1e216028f8ffc7cdbb122aceaa173
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-20 09:23:14 +1100

    added reactive forms

--- src/app/company/company-edit/company-edit.component.ts
+++ src/app/company/company-edit/company-edit.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl } from '@angular/forms';
+import { takeWhile, debounceTime } from 'rxjs/operators';
 
 @Component({
   selector: 'fbc-company-edit',
   templateUrl: './company-edit.component.html',
   styleUrls: ['./company-edit.component.scss']
 })
-export class CompanyEditComponent implements OnInit {
+export class CompanyEditComponent implements OnInit, OnDestroy {
+
 
   constructor() { }
 
+  componentExists = true;
+
+  inputControl: FormControl = new FormControl();
+
   ngOnInit() {
+    console.log(this.inputControl);
+
+    this.inputControl.valueChanges
+    .pipe(
+      takeWhile(t => this.componentExists),
+      debounceTime(2000)
+      )
+    .subscribe(v => console.log('new Value:', v));
   }
 
+  ngOnDestroy(): void {
+    this.componentExists = false;
+  }
 }
