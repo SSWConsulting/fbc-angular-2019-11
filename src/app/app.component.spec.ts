6cc725639f6b27db97a2beebd65cdb8e1d1ca290
brendan richards <brendan_richards_msdn@hotmail.com>
2019-11-20 16:20:21 +1100

    test bed tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,5 +1,24 @@
-import { AppComponent } from './app.component'
+import { AppComponent } from './app.component';
 import { of } from 'rxjs';
+
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { CompanyService } from './company/company.service';
+import { DebugElement } from '@angular/core';
+
+import { CompanyListComponent } from './company/company-list/company-list.component';
+import { CompanyTableComponent } from './company/company-table/company-table.component';
+import { CompanyEditComponent } from './company/company-edit/company-edit.component';
+import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule } from '@angular/common/http';
+import { ReactiveFormsModule } from '@angular/forms';
+import { APP_BASE_HREF } from '@angular/common';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers } from './state';
+import { metaReducers } from './state/index';
+import {By} from '@angular/platform-browser';
+
+
 
 describe(`Component: App Component`, () => {
   it('should add 1+1', () => {
@@ -43,7 +62,6 @@ describe('service mocking', () => {
 
 describe('mock with SpyOn', () => {
 
-
   let component;
   let companySvc;
 
@@ -71,6 +89,71 @@ describe('mock with SpyOn', () => {
     component.companyCount$.subscribe(c => {
       expect(c).toEqual(2);
     });
+  });
+
+});
+
+
+
+
+describe('testBed Tests', () => {
+
+  let fixture: ComponentFixture<AppComponent>;
+  let component: AppComponent;
+  let companySvc;
+  let de: DebugElement;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      declarations: [
+        AppComponent,
+        CompanyListComponent,   // Our routing module needs it
+        CompanyTableComponent,  // Our routing module needs it
+        CompanyEditComponent,   // Our routing module needs it
+        NotFoundComponent
+      ],
+      imports: [
+        AppRoutingModule, // Routerlink in AppComponent needs it
+        HttpClientModule,
+        ReactiveFormsModule,
+        StoreModule.forRoot(reducers, {
+          metaReducers,
+          runtimeChecks: {
+            strictStateImmutability: true,
+            strictActionImmutability: true
+          }
+        }),
+      ],
+      providers: [{provide: APP_BASE_HREF, useValue: '/'}]
+    });
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    companySvc = TestBed.get(CompanyService);
+
+  });
+
+
+  it(`companyCount = 1`, () => {
+
+    expect(companySvc).not.toBeNull();
+
+    spyOn(companySvc, 'getCompanies').and.returnValue(of([
+      {
+        name: 'Fake Company C',
+        email: 'fakeEmail@ssw.com.au',
+        phone: 12345,
+        id: 123
+      }
+    ]));
+    fixture.detectChanges();
+
+    expect(component.companyCount$.subscribe(c => {
+      expect(c).toEqual(1);
+    }));
+
+    let el = fixture.debugElement.query(By.css('#company-count')).nativeElement;
+    expect(el.textContent).toEqual('1');
   });
 
 });
